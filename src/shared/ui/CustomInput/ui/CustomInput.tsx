269fa9c03aa529cc0c41631dca49e158231e287a
Elizaveta Pernikova <lisapernikowa@mail.ru>
2026-01-23 17:47:53 +0500

    feat: add CustomInput

---
+++ src/shared/ui/CustomInput/ui/CustomInput.tsx
@@ -0,0 +1,14 @@
+import { Input } from 'antd';
+
+export const CustomInput = () => {
+
+  return (
+    <Input style={{
+      width: '100%',
+      fontSize: '16px',
+      borderRadius: '20px',
+      padding: '5px 19px',
+      border: '1px solid rgba(0, 0, 0, 0.1)' }}>
+    </Input>
+  );
+};
